d8b173cd215f3e6cead5f272befda10805a8b916
stephingeorge <stephin.g@ewings.ae>
2025-10-28 11:57:56 +0400

    added di and NetworkModule.kt

--- app/src/main/java/com/app/mytasks/di/AppModule.kt
+++ app/src/main/java/com/app/mytasks/di/AppModule.kt
@@ -1,9 +1,12 @@
 package com.app.mytasks.di
 
+import android.content.Context
+import com.app.mytasks.util.ColorPreferences
 import com.google.gson.Gson
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -11,7 +14,13 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 object AppModule {
 
+
     @Provides
     @Singleton
     fun provideGson(): Gson = Gson()
+
+    @Provides
+    fun provideColorPreferences(@ApplicationContext context: Context): ColorPreferences =
+        ColorPreferences(context)
+
 }
